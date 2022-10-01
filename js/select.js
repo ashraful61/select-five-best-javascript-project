0d17faf0593a05240e2eed1202b8c6de01429e08
ashraf_bmqa <ashraful.islam@bengalmobileqa.com>
2022-10-01 17:06:38 +0600

    select functionality added

--- js/select.js
+++ js/select.js
@@ -1,1 +1,30 @@
 
+// Select button event add event listener
+
+const players = document.querySelectorAll('.select-btn')
+
+for (const player of players) {
+
+    player.addEventListener('click', function (event) {
+        // console.log(event.target.parentNode.childNodes[1])
+        const selectedPlayerName = event.target.parentNode.childNodes[1]
+        const selectedPlayers = document.querySelector('#selected-player')
+
+        // console.log(selectedPlayers.childNodes.length)
+        if(selectedPlayers.childNodes.length > 4 ) {
+            alert('You have been already selected 5 players, you can not add more than 5 players.')
+            return
+        }
+
+        //Create list using li tag and insert it to ol tag
+        const createLi = document.createElement('li')
+        createLi.innerText = selectedPlayerName.innerText
+        selectedPlayers.appendChild(createLi)
+
+        // Make disable and background color for recently selected button
+        event.target.setAttribute('disabled', true)
+        event.target.style.backgroundColor = '#A2A9AF'
+    })
+    
+ 
+}
